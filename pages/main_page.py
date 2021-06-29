73fe7bebd11a0056be66c8a394c0461b5de2404e
Yana Revich <yana.arn@gmail.com>
2021-06-29 18:57:46 +0400

    Changes from lesson 4.2, step 8

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
@@ -6,4 +7,4 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR,"#login_link")
         login_link.click() 
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
